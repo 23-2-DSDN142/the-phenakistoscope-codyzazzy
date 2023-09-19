2c1d77fe9cf5e1a7a1a21c71f61ffc47bfd308f6
codyzazzy <85735975+codyzazzy@users.noreply.github.com>
2023-09-19 17:27:31 +1200

    added cloud

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -9,12 +9,13 @@ function setup_pScope(pScope){
   pScope.set_slice_count(SLICE_COUNT); // Set the slice count
   pScope.load_image_sequence("frame", "png", 14); // Load a sequence of images
   pScope.load_image("earth", "png"); // Load a sequence of images
+  pScope.load_image("cloud", "png"); // Load a sequence of images
 }
 //animation wave
 //animation.wave+somhting();
 // Function to set up layers
 function setup_layers(pScope){
-  new PLayer(null, 0,0,100);  // Create a full-circle background layer, ignoring boundaries
+  new PLayer(null, 0,10,80);  // Create a full-circle background layer, ignoring boundaries
  
   // Square layer
   var layer1 = new PLayer(sky);
@@ -22,40 +23,43 @@ function setup_layers(pScope){
   layer1.set_boundary(0, 400); // Set boundary  
 
   // Square layer
-  var layer1 = new PLayer(sky2);
-  layer1.mode(RING); // Set mode to ring
-  layer1.set_boundary(0, 400); // Set boundary    
+  var layer2 = new PLayer(sky2);
+  layer2.mode(RING); // Set mode to ring
+  layer2.set_boundary(400, 600); // Set boundary
+  
+  var layer3 = new PLayer(sky3);
+  layer3.mode(RING); // Set mode to ring
+  layer3.set_boundary(600, 800); // Set boundary    
  
   // Star layer
-  var layer2 = new PLayer(stars); 
-  layer2.mode(RING);  // Set mode to ring
-  layer2.set_boundary(0, 800);  // Set boundary
+  var layer4 = new PLayer(stars); 
+  layer4.mode(RING);  // Set mode to ring
+  layer4.set_boundary(0, 800);  // Set boundary
 
   // Square layer
-  var layer3 = new PLayer(squares);
-  layer3.mode(RING); // Set mode to ring
-  layer3.set_boundary(0, 400); // Set boundary 
-
-  // Square layer
-  var layer4 = new PLayer(animation);
-  layer4.mode(RING); // Set mode to ring
-  layer4.set_boundary(0, 400); // Set boundary  
-
-  var layer5 = new PLayer(earth);
+  var layer5 = new PLayer(animation);
   layer5.mode(RING); // Set mode to ring
   layer5.set_boundary(0, 400); // Set boundary  
 
+  var layer6 = new PLayer(earth);
+  layer6.mode(RING); // Set mode to ring
+  layer6.set_boundary(0, 400); // Set boundary  
+
   // Text layer
-  var layer6 = new PLayer(texts);
-  layer6.mode(SWIRL(1.5)); // Set mode to swirl, parameter is 2
-  layer6.set_boundary(200, 1000); // Set boundary
-  
+  var layer7 = new PLayer(texts);
+  layer7.mode(SWIRL(1.5)); // Set mode to swirl, parameter is 2
+  layer7.set_boundary(200, 1000); // Set boundary
+
+  // Square layer
+  var layer8 = new PLayer(cloud);
+  layer8.mode(RING);
+  layer8.set_boundary( 100, 1050 );
   
 }
 
 // Function to draw stars
 function drawStar(x, y, size, alpha) {
-  fill(192, 192, 192, alpha); // Set fill color and opacity
+  fill(255, 255, 255, alpha); // Set fill color and opacity
   noStroke(); // Remove stroke
   ellipse(x, y, size); // Draw ellipse (circle in this case)
 }
@@ -65,12 +69,6 @@ function stars(x, y, animation, pScope) {
   // Draw multiple stars at specified coordinates
   drawStar(10, -900, 7, 255);
   drawStar(13, -850, 5, 255);
-  // ... (Additional stars can be added here)
-  
-  // Decide opacity based on the current frame
-  //Easing function
-
-  //drawStar(100, -950, 7, alphaForBlinkingStar);  // Draw a star with random opacity
 }
 
 // Function to draw the text layer
@@ -87,13 +85,14 @@ function texts(x, y, animation, pScope){
   text("fill(66, 135, 245)", x-100, y+50);  
   text("print('Hello, World!')", x, y);  
   text("console.log(alpha);", x+100, y-50);  
+  text("bugs", x+130, y-30);  
   
   //console.log("alpha: ", alpha); // Output opacity for debugging
 }
 
 function animation(x, y, animation, pScope){
   scale(0.6); // Scale the drawing area
-  pScope.draw_image_from_sequence("frame", x, 1465-animation.wave()*60, animation.frame); // Draw image sequence
+  pScope.draw_image_from_sequence("frame", x, 1400-animation.wave()*60, animation.frame); // Draw image sequence
 }
 
 // Function to draw the square layer
@@ -112,9 +111,11 @@ function squares(x, y, animation, pScope){
   
 }
 
+
+
 function earth(x, y, animation, pScope){
 // Set background slice angle
-scale(3.4);
+scale(2.5);
 pScope.draw_image("earth",x,y);
 
 }
@@ -125,7 +126,7 @@ function sky(x, y, animation, pScope){
   let backgroundArcEnd = 270 + angleOffset;
 
   
-  fill(0, 0, 280); // Set background color
+  fill(3,33,104); // Set background color
   arc(x,y,1400,1400,backgroundArcStart,backgroundArcEnd); // Draw background slice
   }
 
@@ -135,7 +136,22 @@ function sky2(x, y, animation, pScope){
   let backgroundArcEnd = 270 + angleOffset;
 
   
-  fill(0, 0, 150); // Set background color
+  fill(4,44,128); // Set background color
   arc(x,y,1500,1500,backgroundArcStart,backgroundArcEnd); // Draw background slice
   }
 
+  function sky3(x, y, animation, pScope){
+    let angleOffset = (360 / SLICE_COUNT) / 2;
+    let backgroundArcStart = 270 - angleOffset;
+    let backgroundArcEnd = 270 + angleOffset;
+  
+    
+    fill(5,55,153); // Set background color
+    arc(x,y,1100,1100,backgroundArcStart,backgroundArcEnd); // Draw background slice
+    }
+
+function cloud(x,y,animation,pScope){
+  scale(0.1);
+  //pScope.draw_image("cloud",animation.wave(1)*200,-1800-animation.wave(1)*200);
+  pScope.draw_image("cloud", x, 4000-animation.wave()*500, animation.frame); 
+}
